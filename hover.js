e0bc65170c99f5a3f245c7aa856575d1a818d6aa
SurDno <surdno@yandex.ru>
2023-11-13 11:38:41 +0000

    Site redone

---
+++ hover.js
@@ -0,0 +1,31 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var logos = document.querySelectorAll('.logos img');
+    var hintElement = document.getElementById('hint');
+
+    function showHint(logo) {
+        var logoName = logo.getAttribute('name');
+		
+		if(logoName == "unity") 
+			hintElement.textContent = "This project was developed with Unity game engine.";
+		else if(logoName == "csharp") 
+			hintElement.textContent = "This project was developed with C# programming language.";
+		else if(logoName == "mirror") 
+			hintElement.textContent = "This project was developed with Mirror networking library.";
+		else if(logoName == "github") 
+			hintElement.textContent = "This project was developed with GitHub version control system.";
+    }
+
+    function resetHint() {
+        hintElement.textContent = '';
+    }
+
+    logos.forEach(function(logo) {
+        logo.addEventListener('mouseover', function() {
+            showHint(logo);
+        });
+
+        logo.addEventListener('mouseout', function() {
+            resetHint();
+        });
+    });
+});
